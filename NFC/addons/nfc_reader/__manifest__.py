b008c4aca16ca16fd492acafef98d54f84cbf711
Oscar Rodríguez Dorta <or979243@gmail.com>
2026-02-12 09:33:12 +0000

    main
    Conectando el lector de tarjeta con odoo

---
+++ NFC/addons/nfc_reader/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "NFC Reader",
+    "version": "1.0",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+}
--- NFC/prueba_uid.py
+++ NFC/prueba_uid.py
@@ -7,20 +7,36 @@ buffer = ""
 
 ODOO_URL = "http://localhost:8069/nfc/read"
 
+
+def enviar_uid(uid):
+    try:
+        response = requests.post(
+            ODOO_URL,
+            json={
+                "jsonrpc": "2.0",
+                "method": "call",
+                "params": {"uid": uid},
+                "id": 1,
+            },
+            timeout=5,
+        )
+
+        data = response.json()
+        print("Respuesta de Odoo:", data)
+
+    except Exception as e:
+        print("Error enviando a Odoo:", e)
+
+
 while True:
     event = keyboard.read_event()
 
     if event.event_type == keyboard.KEY_DOWN:
+
         if event.name == "enter":
             if buffer:
                 print("UID leído:", buffer)
-
-                try:
-                    r = requests.post(ODOO_URL, json={"uid": buffer})
-                    print("Respuesta de Odoo:", r.text)
-                except Exception as e:
-                    print("Error enviando a Odoo:", e)
-
+                enviar_uid(buffer)
                 buffer = ""
 
         elif len(event.name) == 1:
