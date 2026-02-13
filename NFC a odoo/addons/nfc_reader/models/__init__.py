1ec33edc492f8ab5eae77705c2ea524271c00de8
Oscar Rodríguez Dorta <or979243@gmail.com>
2026-02-13 12:40:34 +0000

    main
    Vamos ya a importar en la base de  datos  de odoo a los alumnos

--- NFC a odoo/addons/nfc_reader/models/__init__.py
+++ NFC a odoo/addons/nfc_reader/models/__init__.py
@@ -1,1 +1,2 @@
 from . import nfc_log
+from . import alumno
---
+++ NFC a odoo/addons/nfc_reader/models/alumno.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class Alumno(models.Model):
+    _name = "nfc.alumno"
+    _description = "Alumno con tarjeta NFC"
+
+    nombre = fields.Char(string="Nombre", required=True)
+    dni = fields.Char(string="DNI")
+    cial = fields.Char(string="CIAL")
+    curso = fields.Char(string="Curso")
+    uid_nfc = fields.Char(string="UID NFC", required=True)
