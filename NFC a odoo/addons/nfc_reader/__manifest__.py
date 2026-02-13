3af24ea3e7bd02d056207b2c718a99dd54f8b89d
Oscar Rodríguez Dorta <or979243@gmail.com>
2026-02-13 13:20:36 +0000

    Visibilidad Alumno en odoo
    Para que alumno sea visible en odoo se puso un xml y se modifico el manifest

--- NFC a odoo/addons/nfc_reader/__manifest__.py
+++ NFC a odoo/addons/nfc_reader/__manifest__.py
@@ -2,6 +2,9 @@
     "name": "NFC Reader",
     "version": "1.0",
     "depends": ["base"],
+    "data": [
+        "views/alumno_views.xml",
+    ],
     "installable": True,
     "application": False,
 }
