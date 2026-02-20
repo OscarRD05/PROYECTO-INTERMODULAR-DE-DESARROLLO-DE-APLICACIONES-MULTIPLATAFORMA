0e6db87201b8616bb48cd8353df157c25a9e651d
Oscar Rodríguez Dorta <or979243@gmail.com>
2026-02-20 11:51:01 +0000

    Delete NFC a odoo/addons/nfc_reader/models/nfc_log.py

--- NFC a odoo/addons/nfc_reader/models/nfc_log.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class NFCLog(models.Model):
-    _name = "nfc.log"
-    _description = "Registro NFC"
-
-    uid = fields.Char(string="UID")
-    date = fields.Datetime(default=fields.Datetime.now)
