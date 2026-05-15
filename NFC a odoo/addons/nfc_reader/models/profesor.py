9b261e0c52ba67bfbeccf606c3fb5f005da48c44
EliN4til <joelito1gs@gmail.com>
2026-05-15 11:30:25 +0100

    Añadido validación del formato de DNI/NIE en el modelo Profesor. Se implementaron restricciones para asegurar que el DNI/NIE ingresado cumpla con el formato correcto y se manejen errores mediante excepciones de validación.

--- NFC a odoo/addons/nfc_reader/models/profesor.py
+++ NFC a odoo/addons/nfc_reader/models/profesor.py
@@ -1,4 +1,8 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import re
+
+LETRAS_DNI = "TRWAGMYFPDXBNJZSQVHLCKE"
 
 
 class Profesor(models.Model):
@@ -22,6 +26,30 @@ class Profesor(models.Model):
         "tutor_id",
         string="Cursos / Grupos",
     )
+
+    @api.constrains('dni_nie')
+    def _check_dni_nie(self):
+        for rec in self:
+            if not rec.dni_nie:
+                continue
+            dni = rec.dni_nie.upper().strip()
+            if not re.match(r'^[0-9]{8}[A-Z]$|^[XYZ][0-9]{7}[A-Z]$', dni):
+                raise ValidationError(
+                    "El DNI/NIE '%s' no tiene un formato válido.\n"
+                    "DNI: 8 dígitos + 1 letra (ej: 12345678A)\n"
+                    "NIE: Letra X/Y/Z + 7 dígitos + 1 letra (ej: X1234567A)"
+                    % rec.dni_nie
+                )
+            if dni[0] in 'XYZ':
+                numero = int(str('XYZ'.index(dni[0])) + dni[1:8])
+            else:
+                numero = int(dni[:8])
+            letra_correcta = LETRAS_DNI[numero % 23]
+            if dni[-1] != letra_correcta:
+                raise ValidationError(
+                    "La letra del DNI/NIE '%s' no es correcta. "
+                    "Debería ser '%s'." % (rec.dni_nie, letra_correcta)
+                )
 
     def _sync_uid_to_employee(self):
         for rec in self:
