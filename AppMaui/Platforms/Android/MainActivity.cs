562610da5142533ab90c88bcf57d2cee79cc5c42
Oscar Rodríguez Dorta <or979243@gmail.com>
2026-05-15 11:29:46 +0100

    MauiApp

--- AppMaui/Platforms/Android/MainActivity.cs
+++ AppMaui/Platforms/Android/MainActivity.cs
@@ -1,11 +1,33 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.NFC;
 
-namespace App
+namespace App;
+
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    LaunchMode = LaunchMode.SingleTask,
+    ConfigurationChanges = ConfigChanges.ScreenSize
+                         | ConfigChanges.Orientation
+                         | ConfigChanges.UiMode
+                         | ConfigChanges.ScreenLayout
+                         | ConfigChanges.SmallestScreenSize
+                         | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
+    protected override void OnCreate(Bundle savedInstanceState)
     {
+        base.OnCreate(savedInstanceState);
+
+        CrossNFC.Init(this);
+    }
+
+    protected override void OnNewIntent(Android.Content.Intent intent)
+    {
+        base.OnNewIntent(intent);
+
+        CrossNFC.OnNewIntent(intent);
     }
 }
